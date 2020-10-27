99be317a4792984a62648080de607ab97569ab3c
wmt_rn_safid <safid@webmob.tech>
2020-10-27 19:41:53 +0530

    Matche details screen

--- Password-Generator/Cricbuzz/src/screens/MatchDetails/index.js
+++ Password-Generator/Cricbuzz/src/screens/MatchDetails/index.js
@@ -25,6 +25,32 @@ const MatchDetails = ({navigation, route}) => {
             });
     };
 
+    const getMatchesDetailsData = (matchId) => {
+        axios
+            .get(`http://mapps.cricbuzz.com/cbzios/match/${matchId}`)
+            .then((res) => {
+                console.log(res,"matchId response");
+                if (res && res.data && res.data.matches) {
+                    // setMatchesList(res.data.matches);
+                } else {
+                    // setMatchesList([]);
+                }
+            });
+    };
+
+    const getMatchesDetailsByBall = (matchId) => {
+        axios
+            .get(`http://mapps.cricbuzz.com/cbzios/match/${matchId}`)
+            .then((res) => {
+                console.log(res,"matchId response");
+                if (res && res.data && res.data.matches) {
+                    // setMatchesList(res.data.matches);
+                } else {
+                    // setMatchesList([]);
+                }
+            });
+    };
+
     return (
         <View style={styles.container}>
             <Text>{'Match Details'}</Text>
